b7dea06154a9a2b95eb7e163684ead15ddebfd17
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 13:49:32 -0500

    Uploading homework that wasn't working at the time so I can track getting it to a working state.

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Excercise 2.4
+        Scanner number = new Scanner(System.in);
+
+        System.out.println("Please enter a number. ");
+        double userNumber = number.nextInt();
+
+        do {
+            if (userNumber % 2 == 0) {
+                System.out.println("The number you entered is even.");
+            } else {
+                System.out.println("The number you entered is odd.");
+            }
+            int num2;
+            boolean response = true;
+
+
+
+    }
+}
+
+do {would you like another?
+
+        System.out.println("Would you like to enter another number?  (Enter Y or N)")
+        boolean answer = true;
+    if response = yes
+                then response = true
